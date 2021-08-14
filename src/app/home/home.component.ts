d923eadb5b6bc590b26471a5f32f2f377e6ac89d
Pratik Bhudke <pratik.bhudke007@gmail.com>
2021-08-14 19:53:04 +0530

    Task - Custom data file upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataManagementService } from '../shared/services/data-management.service';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,25 @@ export class HomeComponent implements OnInit {
   isSample: boolean;
   fileToUpload: File | null = null;
   
-  constructor(private router: Router) {
+  constructor(private router: Router, private dataManagementService: DataManagementService) {
     this.isSample = true;
   }
 
   handleFileInput(files: FileList) {
-    this.fileToUpload = files.item(0);
+    this.fileToUpload = files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsText(this.fileToUpload, "UTF-8");
+    fileReader.onload = () => {
+      const fileData = JSON.parse(fileReader.result as string);
+      if("data" in fileData) {
+        this.dataManagementService.data = fileData;
+      } else {
+        alert("No data in File");
+      }
+    }
+    fileReader.onerror = (error) => {
+      console.log(error);
+    }
   }
 
   sampleDataToggle(value) {
